eb000bbf18a6393ba2d1585e306aa7ae0ff25e8c
Sierra Wang <sierrawang97@gmail.com>
2024-01-25 10:35:07 -0800

    Update icons

--- src/components/PaperList.jsx
+++ src/components/PaperList.jsx
@@ -6,24 +6,24 @@ export const PaperList = () => {
     return (
         <div className="paper-list">
             <Paper
-                authors={"S. Wang, J. Mitchell, C. Piech"}
+                authors={"S. Wang*, J. Mitchell, C. Piech"}
                 title={"A Large Scale RCT on Effective Error Messages in CS1"}
                 conference={"SIGCSE 2024"}
                 award={"Best Paper Award"}
-                picUrl={"error_messages_pic.png"}
+                picUrl={"error_messages_pic.jpeg"}
              />
             <Paper
-                authors={"S. Wang, J. Mitchell, N. Haber, C. Piech"}
+                authors={"S. Wang*, J. Mitchell, N. Haber, C. Piech"}
                 title={"Math IDE: A Platform for Creating with Math"}
                 conference={"SIGCSE 2024"}
-                picUrl={"math_pic.png"}
+                picUrl={"math_pic.jpeg"}
              />
             <Paper
-                authors={"S. Wang, F. Bakir, T. Ekaireb, J. Pearson, C. Krintz, R. Wolski"}
+                authors={"S. Wang*, F. Bakir, T. Ekaireb, J. Pearson, C. Krintz, R. Wolski"}
                 title={"MSDBench: Understanding the Performance Impact of Isolation Domains on Microservice-based IoT Deployments"}
                 conference={"Bench 2022"}
                 award={"Tony Hey Best Student Paper Award"}
-                picUrl={"microservices_pic.png"}
+                picUrl={"microservices_pic.jpeg"}
              />
         </div>
     )
